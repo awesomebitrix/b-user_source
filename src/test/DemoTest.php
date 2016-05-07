d2308e272b3dbe15479043f4bd531407c66f0146
Sergey Gripinskiy <web-architect@mail.ru>
2016-05-07 08:23:17 +0300

    Добавлен PHPUnit

---
+++ src/test/DemoTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gripinskiy
+ * Date: 07.05.16
+ * Time: 8:07
+ */
+
+namespace webarch\buser\test;
+
+
+class DemoTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDemo()
+    {
+        $this->assertEquals(true, true, "Just ensure true is true");
+    }
+}
